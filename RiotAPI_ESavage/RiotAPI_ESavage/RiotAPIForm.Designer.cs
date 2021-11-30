c826565dd4e99f314f90cc516e6d081107a99ec9
EsavUCF <70274517+EsavUCF@users.noreply.github.com>
2021-11-30 11:34:27 -0500

    GUI Created. (Lacking implementation)

--- RiotAPI_ESavage/RiotAPI_ESavage/RiotAPIForm.Designer.cs
+++ RiotAPI_ESavage/RiotAPI_ESavage/RiotAPIForm.Designer.cs
@@ -30,21 +30,59 @@ namespace RiotAPI_ESavage
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GetAPIData = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.UsernameBar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // GetAPIData
+            // 
+            resources.ApplyResources(this.GetAPIData, "GetAPIData");
+            this.GetAPIData.Name = "GetAPIData";
+            this.GetAPIData.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // UsernameBar
+            // 
+            resources.ApplyResources(this.UsernameBar, "UsernameBar");
+            this.UsernameBar.Name = "UsernameBar";
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.UsernameBar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.GetAPIData);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button GetAPIData;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label UsernameBar;
     }
 }
 
