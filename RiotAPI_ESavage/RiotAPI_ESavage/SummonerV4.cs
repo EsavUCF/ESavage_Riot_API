7edaa0606058966830c867454615aa1440e8050e
EsavUCF <70274517+EsavUCF@users.noreply.github.com>
2021-11-30 12:06:49 -0500

    Summoner Class Created.

---
+++ RiotAPI_ESavage/RiotAPI_ESavage/SummonerV4.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace RiotAPI_ESavage
+{
+    class Summoner
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("accountId")]
+        public string AccountID { get; set; }
+
+        [JsonProperty("puuid")]
+        public string PuuID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("profileIconId")]
+        public int ProfileIconId { get; set; }
+
+        [JsonProperty("revisionDate")]
+        public long RevisionDate { get; set; }
+
+        [JsonProperty("summonerLevel")]
+        public long SummerLevel { get; set; }
+    }
+}
